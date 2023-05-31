50d1f350ded63260448f77f4c18d73aed22c1d37
jballard86 <jeff.ballard.86@gmail.com>
2023-05-31 18:49:23 -0400

    fillet

---
+++ src/Fillet.h
@@ -0,0 +1,11 @@
+/*
+Calculate the X/Y coords by using sine cosine
+store the result in an array
+the total number of array positions equals the amount of steps to take, this should be a user input
+calculate the arrays before starting the spindle
+have the user place the tool on the end of the work, tool position will create an origin of x == r and y == -radius
+
+user input: steps, radius, depth of cut
+
+
+*/
